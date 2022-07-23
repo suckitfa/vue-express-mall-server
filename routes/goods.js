7fae0015df0961ee6aed68d2a1c101deb82bf46b
Bob Tang <254202042@qq.com>
2022-07-23 18:25:39 +0800

    bufix: redo the data

--- routes/goods.js
+++ routes/goods.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 var goods = require('../mock/goods.json')
 /* GET home page. */
-router.get('/goodsList', function(req, res, next) {
-    res.render('index',{
-        name:"fuck"
-    })
+router.get('/', function(req, res, next) {
+    res.json(goods)
 });
 
 module.exports = router;
