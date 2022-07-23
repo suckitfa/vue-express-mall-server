67afc9d78fc7ae33a598b21b192f9889ea4272d5
Bob Tang <254202042@qq.com>
2022-07-23 19:14:42 +0800

    feat: change the goods router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,8 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/goodsList',goodsRouter)
+app.use('/goods',goodsRouter)
 module.exports = app;
