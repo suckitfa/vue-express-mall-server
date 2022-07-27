b3d1b72a5aca723f3ab3f31603acecba6e7d2ead
suckitfa <254202042@qq.com>
2022-07-27 10:46:33 +0800

    feat: goods list api supporting panagation

--- routes/goods.js
+++ routes/goods.js
@@ -11,26 +11,48 @@ mongoose.connection.on('disconnected',() => console.log("链接断开"))
 router.get('/', function(req, res, next) {
     // 获取参数sort
     let page = req.param("page")
+    let pages = req.param("page")
+    console.log('pages = ',pages)
     let pageSize = req.param("page")
     let sort = req.param("sort")
     let skip = (page - 1) * pageSize; // 分页计算公式
-    // console.log(`page = ${page}, pageSize = ${pageSize},sort = ${sort}`)
-    // var params = {}
-    // var goodsModel = Goods.find(params).skip(skip).limit(pageSize);
-    // goodsModel.sort({'salePrice':sort});
-    // goodsModel.exec({},(err,doc) => {
-    //     if (err) {
-    //     } else {
-
-    //     }
-    // })
-    // 查找所有的商品，返回商品列表
-    Goods.find({},(err,doc) => {
+    let priceLevel = req.param('priceLevel');
+    let priceGt = '',priceLte = '';
+    let params = {}
+    if (priceLevel !== 'all') {
+        switch(priceLevel) {
+            case '0':
+                priceGt = 0;
+                priceLte = 100;
+                break;
+            case '1':
+                priceGt = 100;
+                priceLte = 500;
+                break;
+            case '2':
+                priceGt = 500;
+                priceLte = 1000;
+                break;
+            case '3':
+                priceGt = 1000;
+                priceLte = 5000;
+        }
+        // 构造
+        params = {
+            salePrice: {
+                $gt:priceGt,
+                $lte:priceLte
+            }
+        }
+    }
+    var goodsModel = Goods.find(params).skip(skip).limit(pageSize);
+    goodsModel.sort({'salePrice':sort});
+    goodsModel.exec({},(err,doc) => {
         if (err) {
             res.json({
-                status:'1',
+                status:1,
                 msg:err.message
-            }) 
+            })
         } else {
             res.json({
                 status:0,
@@ -42,6 +64,24 @@ router.get('/', function(req, res, next) {
             })
         }
     })
+    // 查找所有的商品，返回商品列表
+    // Goods.find({},(err,doc) => {
+    //     if (err) {
+    //         res.json({
+    //             status:'1',
+    //             msg:err.message
+    //         }) 
+    //     } else {
+    //         res.json({
+    //             status:0,
+    //             msg:"",
+    //             result:{
+    //                 count:doc.length,
+    //                 list:doc,
+    //             }
+    //         })
+    //     }
+    // })
 });
 
 module.exports = router;
