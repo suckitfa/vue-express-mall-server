e571e5615972c18c09b4b737eef72340daae0a7e
suckitfa <254202042@qq.com>
2022-08-02 11:07:45 +0800

    add cors options

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+const corsOptions = {
+    origin:'http://localhost:8080',
+    credentials:true,
+}
+app.use(cors(corsOptions))
 // 拦截用户是否登入
 app.use(function(req,res,next) {
     if (req.cookies.userId) {
