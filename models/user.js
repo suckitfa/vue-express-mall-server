94ed35179c4799d6417461147bd25c83eb4f5c8a
suckitfa <254202042@qq.com>
2022-08-03 14:40:17 +0800

    change: change user model

--- models/user.js
+++ models/user.js
@@ -15,6 +15,15 @@ var userSchema = new mongoose.Schema({
             "productNum":String,
         }
     ],
-    "addressList":Array
+    "addressList":[
+        {
+            "addressId": String,
+            "userName": String,
+            "streetName": String,
+            "postCode": String,
+            "tel": String,
+            "isDefault": Boolean
+        }
+    ]
 });
 module.exports = mongoose.model('User',userSchema);
